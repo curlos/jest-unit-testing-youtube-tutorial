6100d4f151585f25ba651db57b03f41b99e0fdcd
Carlos Martinez <curlosmart@gmail.com>
2024-12-24 11:43:28 -0500

    Finish writing all of the tests.

--- utils/helper.js
+++ utils/helper.js
@@ -1,3 +1,11 @@
 export const sum = (a, b) => {
     return a + b;
 }
+
+export const deleteUserById = (array = [], id = "") => {
+    return array.filter(user => user.id !== id)
+}
+
+export const findUserById = (array = [], id = "") => {
+    return array.find((user) => user.id === id)
+}
